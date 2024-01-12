8ab336471cb8024102957e3e6636853723d0c3bf
Matorykin Sergey <s.matorykin@olp.ru>
2024-01-12 17:29:21 +0500

    Added command Create Package and send to test

--- src/extension.ts
+++ src/extension.ts
@@ -67,8 +67,61 @@ export function activate(context: vscode.ExtensionContext) {
 			terminal.show(true);
 			//terminal.sendText("clio generate-pkg-zip " + uri.fsPath + " -d " + path.join(outputPath, parentDirectory + ".gz"));
 			terminalLog.appendLine('Execute: clio generate-pkg-zip ' + uri.fsPath + ' -d ' + path.join(outputPath, parentDirectory + '.gz'));
+			try{
+				const result = await execShell("clio generate-pkg-zip " + uri.fsPath + " -d " + path.join(outputPath, parentDirectory + ".gz"));
+				terminalLog.appendLine('Result: ' + result);
+				if(result.includes('Done')){
+					//vscode.commands.executeCommand(`vscode.openFolder`, outputPath);
+				}
+			}
+			catch(error){
+				terminalLog.appendLine('Error: ' + error);
+			}
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('bpmsoft-creator-package.test.createandsend', async (uri:vscode.Uri) => {
+		console.log(uri.fsPath);
+		
+		const path = require("path");
+		const parentDirectory = path.basename(uri.fsPath);
+		
+		const config = vscode.workspace.getConfiguration('bcp');
+		const outputPath = config.get('outputPath');
+		const remoteServer = config.get('remoteServer');
+		const remoteServerLogin = config.get('remoteServerLogin');
+		const remoteServerPassword = config.get('remoteServerPassword');
+
+		if(isPermittedBranch()){
+			const terminal = vscode.window.createTerminal(`Bpmsoft Terminal`);
+			terminal.show(true);
+			//terminal.sendText("clio generate-pkg-zip " + uri.fsPath + " -d " + path.join(outputPath, parentDirectory + ".gz"));
+			terminalLog.appendLine('Execute: clio generate-pkg-zip ' + uri.fsPath + ' -d ' + path.join(outputPath, parentDirectory + '.gz'));
 			const result = await execShell("clio generate-pkg-zip " + uri.fsPath + " -d " + path.join(outputPath, parentDirectory + ".gz"));
 			terminalLog.appendLine('Result: ' + result);
+			if(result.includes('Done')){
+				//vscode.commands.executeCommand(`vscode.openFolder`, outputPath);
+				terminalLog.appendLine(
+					'Execute: clio push-pkg ' 
+					+ path.join(outputPath, parentDirectory + ".gz") 
+					+ ' -u ' + remoteServer 
+					+ ' -l ' + remoteServerLogin 
+					+ ' -p ' + remoteServerPassword
+				);
+				try{
+					const pushResult = await execShell(
+						'Execute: clio push-pkg ' 
+						+ path.join(outputPath, parentDirectory + ".gz") 
+						+ ' -u ' + remoteServer 
+						+ ' -l ' + remoteServerLogin 
+						+ ' -p ' + remoteServerPassword
+					);
+					terminalLog.appendLine('Result: ' + pushResult);
+				}
+				catch(error){
+					terminalLog.appendLine('Error: ' + error);
+				}
+			}
 		}
 	}));
 }
