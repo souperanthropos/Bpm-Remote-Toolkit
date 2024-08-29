ff07e09bc8ef803ea34a4965f9e537c544f87c4e
Matorykin Sergey <s.matorykin@olp.ru>
2024-08-29 13:24:12 +0500

    fix update time in method UpdateTimeInDescriptor

--- src/managers/filemanager.ts
+++ src/managers/filemanager.ts
@@ -11,8 +11,15 @@ export class FileManager {
     
             const readData = await vscode.workspace.fs.readFile(vscode.Uri.file(descriptorFile));
             let readStr = new TextDecoder('utf-8').decode(readData);
-            readStr = readStr.replace('"ModifiedOnUtc": "\\/Date(1672091018000)\\/"', `"ModifiedOnUtc": "\\/Date(${Date.now()})\\/"`);
-            await vscode.workspace.fs.writeFile(vscode.Uri.file(descriptorFile), new TextEncoder().encode(readStr));
+
+            const pattern = /("ModifiedOnUtc": "\\\/Date\()([0-9]+)(\)\\\/")/;
+            var updateStr = readStr.replace(pattern, `$1${this.getUnixTimeWithoutMilliseconds()}$3`);
+            await vscode.workspace.fs.writeFile(vscode.Uri.file(descriptorFile), new TextEncoder().encode(updateStr));
         }
+    }
+
+    getUnixTimeWithoutMilliseconds(): string {
+        const time = Math.floor(Date.now() / 1000) * 1000;
+        return time.toString();
     }
 }
