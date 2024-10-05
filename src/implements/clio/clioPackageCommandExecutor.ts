a1ec1ff8ee7760e8438890e45e51b56cb535aeae
Matorykin Sergey <s.matorykin@olp.ru>
2024-10-05 19:26:23 +0500

    added method addTextToLogFile in TerminalWrapper

--- src/implements/clio/clioPackageCommandExecutor.ts
+++ src/implements/clio/clioPackageCommandExecutor.ts
@@ -8,6 +8,7 @@ export class ClioPackageCommandExecutor implements IPackageCommandExecutor {
     constructor(private terminal: TerminalWrapper) {}
 
     public async createPackage(targetFolderPath: string, fullPathFile: string): Promise<boolean> {
+        await this.terminal.addTextToLogFile('Start package creating...');
         await this.terminal.executeCommand('del ' + fullPathFile);
         return await this.terminal.executeCommand(
             "clio generate-pkg-zip "
@@ -19,6 +20,7 @@ export class ClioPackageCommandExecutor implements IPackageCommandExecutor {
     public async pushPackage(pkg: packageSettings): Promise<boolean> {
         const path = require("path");
         const packageFilePath = path.join(ExtensionSettings.outputPath(FolderType.package), getDirectoryName(pkg.targetFolderPath) + ".gz");
+        await this.terminal.addTextToLogFile('Start package uploading...');
         return await this.terminal.executeCommand(
             'clio push-pkg '
             + packageFilePath
