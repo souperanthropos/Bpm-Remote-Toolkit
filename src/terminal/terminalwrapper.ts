6a6a294775d4ccdbd07425e4b3b97edd9ded5d05
Matorykin Sergey <s.matorykin@olp.ru>
2024-10-13 15:06:03 +0500

    logging improvements

--- src/terminal/terminalwrapper.ts
+++ src/terminal/terminalwrapper.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { Logger } from '../common/logger';
 
 export abstract class TerminalWrapper {
-	abstract addTextToLogFile(message: string): Promise<boolean>;
+	abstract addTextToLogFile(message: string, writeTimestamp: boolean): Promise<boolean>;
 	abstract executeCommand(command: string): Promise<boolean>;
 }
 
@@ -19,8 +19,12 @@ export class PowerShellWrapper extends TerminalWrapper {
 		this.terminalName = Logger.terminalName;
 	}
 
-	public async addTextToLogFile(message: string): Promise<boolean> {
-		return await this.executeCommand(`$content =  (Get-Date -Format "dd/MM/yyyy HH:mm:ss.fff").ToString() + ' - ${message}'; $content >> ${this.executeLogFilePath}`);
+	public async addTextToLogFile(message: string, writeTimestamp: boolean): Promise<boolean> {
+		if(writeTimestamp){
+			return await this.executeCommand(`$content =  (Get-Date -Format "dd/MM/yyyy HH:mm:ss.fff").ToString() + ' - ${message}'; $content >> ${this.executeLogFilePath}`);
+		}else{
+			return await this.executeCommand(`$content = ${message}'; $content >> ${this.executeLogFilePath}`);
+		}
 	}
 
 	public async executeCommand(command: string): Promise<boolean> {
