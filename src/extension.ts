bf5336cc7cfac846e74893f29ca385a0727c7ad6
Matorykin Sergey <s.matorykin@olp.ru>
2025-02-26 19:48:01 +0500

    Refactor BaseWebAppManager to integrate FileManager for log file deletion; update webAppUnregister method signatures

--- src/extension.ts
+++ src/extension.ts
@@ -81,7 +81,7 @@ function registerButtonCommands(context: vscode.ExtensionContext){
 							message: 'check credential failed',
 							increment: 50
 						});
-						await BpmToolkit.Instance.webAppManager.webAppUnregister(server, false);
+						await BpmToolkit.Instance.webAppManager.webAppUnregister(server);
 						context.globalState.update(server.id, false);
 					}
 					vscode.commands.executeCommand('bpmEnvironments.refreshEntry');
@@ -91,7 +91,8 @@ function registerButtonCommands(context: vscode.ExtensionContext){
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('bpmEnvironments.server.unregister', async (server: enviromentSettings) => {
-		await BpmToolkit.Instance.webAppManager.webAppUnregister(server, true);
+		await BpmToolkit.Instance.fileManager.DeleteFile(Logger.getExecuteLogFilePath());
+		await BpmToolkit.Instance.webAppManager.webAppUnregister(server);
 		context.globalState.update(server.id, false);
 		vscode.commands.executeCommand('bpmEnvironments.refreshEntry');
 	}));
