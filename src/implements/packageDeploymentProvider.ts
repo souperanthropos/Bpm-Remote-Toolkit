044ce003e68a9b6e5657d5916323f04f9f956b52
Matorykin Sergey <s.matorykin@olp.ru>
2025-05-29 13:02:13 +0500

    Updated visualization in Package Deployment Management

--- src/implements/packageDeploymentProvider.ts
+++ src/implements/packageDeploymentProvider.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { queueItem } from '../interfaces';
 import { PackageSettings } from '../common/packageSettings';
+import { DeployNode, DeployStatus } from '../managers/packageDeploymentManager';
 
 export class PackageDeploymentProvider implements vscode.TreeDataProvider<queueItem>, vscode.TreeDragAndDropController<queueItem> {
 	dropMimeTypes = ['application/vnd.code.tree.packageDeploymentManagement', 'application/vnd.code.tree.packagesExplorer'];
@@ -9,7 +10,8 @@ export class PackageDeploymentProvider implements vscode.TreeDataProvider<queueI
     private _onDidChangeTreeData: vscode.EventEmitter<queueItem | undefined | void> = new vscode.EventEmitter<queueItem | undefined | void>();
 	readonly onDidChangeTreeData: vscode.Event<queueItem | undefined | void> = this._onDidChangeTreeData.event;
 
-    private _queueItems!: queueItem[];
+    //private _queueItems!: queueItem[];
+	private _deployNode!: DeployNode;
 
 	handleDrag?(source: readonly queueItem[], dataTransfer: vscode.DataTransfer, token: vscode.CancellationToken): Thenable<void> | void {
 		dataTransfer.set('application/vnd.code.tree.packageDeploymentManagement', new vscode.DataTransferItem(source));
@@ -25,9 +27,9 @@ export class PackageDeploymentProvider implements vscode.TreeDataProvider<queueI
 		}
 		let targetIndex = 0;
 		if(target !== undefined){
-			targetIndex = this._queueItems.findIndex(q=>q === target);
+			targetIndex = this._deployNode.packages.findIndex(q=>q === target);
 		}else{
-			targetIndex = this._queueItems.length - 1;
+			targetIndex = this._deployNode.packages.length - 1;
 		}
 		treeItems.forEach(element => {
 			this.itemMove(element, targetIndex);
@@ -35,8 +37,8 @@ export class PackageDeploymentProvider implements vscode.TreeDataProvider<queueI
 		this._onDidChangeTreeData.fire();
 	}
 
-    refresh(items: ReadonlyArray<queueItem>): void {
-		this._queueItems = items.concat();
+	refresh(node: DeployNode): void {
+		this._deployNode = node;
 		this._onDidChangeTreeData.fire();
 	}
 
@@ -49,6 +51,20 @@ export class PackageDeploymentProvider implements vscode.TreeDataProvider<queueI
 				vscode.TreeItemCollapsibleState.Expanded
 			);
 			treeItem.tooltip = 'packages.zip';
+			switch(this._deployNode.status){
+				case DeployStatus.Uploading:
+					treeItem.iconPath = new vscode.ThemeIcon('loading~spin');
+					break;
+				case DeployStatus.Success:
+					treeItem.iconPath = new vscode.ThemeIcon('package', new vscode.ThemeColor("packageDeploymentManagement.successCompleted"));
+					break;
+				case DeployStatus.Error:
+					treeItem.iconPath = new vscode.ThemeIcon('package', new vscode.ThemeColor("packageDeploymentManagement.errorCompleted"));
+					break;
+				default:
+					treeItem.iconPath = new vscode.ThemeIcon('package');
+					break;
+			}
 		}else{
 			treeItem = new queueTreeItem(
 				element.package.packageFileName,
@@ -57,42 +73,37 @@ export class PackageDeploymentProvider implements vscode.TreeDataProvider<queueI
 				element.package
 			);
 			treeItem.tooltip = element.package.targetFolderPath;
-		}
-
-		if(element.isRunning){
-			treeItem.iconPath = new vscode.ThemeIcon('loading~spin');
-		}else{
-			if(element.Completed){
-				if(element.Completed.isSuccess){
-					treeItem.iconPath = new vscode.ThemeIcon('package', new vscode.ThemeColor("packageDeploymentManagement.successCompleted"));
-				}else{
-					treeItem.iconPath = new vscode.ThemeIcon('package', new vscode.ThemeColor("packageDeploymentManagement.errorCompleted"));
-				}
+			if(element.isRunning){
+				treeItem.iconPath = new vscode.ThemeIcon('loading~spin');
 			}else{
-				treeItem.iconPath = new vscode.ThemeIcon('package');
+				if(element.Completed){
+					if(element.Completed.isSuccess){
+						treeItem.iconPath = new vscode.ThemeIcon('package', new vscode.ThemeColor("packageDeploymentManagement.successCompleted"));
+					}else{
+						treeItem.iconPath = new vscode.ThemeIcon('package', new vscode.ThemeColor("packageDeploymentManagement.errorCompleted"));
+					}
+				}else{
+					treeItem.iconPath = new vscode.ThemeIcon('package');
+				}
 			}
 		}
 		return treeItem;
 	}
 
     getChildren(element?: queueItem): Thenable<queueItem[]> {
-		if (!element && this._queueItems.length === 0) {
+		if (!this._deployNode) {
 			return Promise.resolve([]);
 		}
-		if(element || this._queueItems.length === 1){
-			return Promise.resolve(this._queueItems);
+		if(!element && this._deployNode.packages.length > 1){
+			return Promise.resolve(this._deployNode.getRootNodes());
 		}
-		return Promise.resolve([{
-			package: undefined,
-			isRunning: false,
-			Completed: null
-		}]);
+		return Promise.resolve(this._deployNode.packages);
 	}
 
 	private itemMove(element: queueItem, newIndex: number) {
-		var oldIndex = this._queueItems.findIndex(q=>q === element);
-		this._queueItems.splice(oldIndex, 1);
-		this._queueItems.splice(newIndex, 0, element);
+		var oldIndex = this._deployNode.packages.findIndex(q=>q === element);
+		this._deployNode.packages.splice(oldIndex, 1);
+		this._deployNode.packages.splice(newIndex, 0, element);
 	}
 }
 
